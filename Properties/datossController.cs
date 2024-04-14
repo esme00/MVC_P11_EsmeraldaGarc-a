2b37a015a1104de9380f7f4787e55e19a3fbaf7a
Esme Garcìa <esmeralda.garcia1@catolica.edu.sv>
2024-04-14 15:46:16 -0600

    Agredado al controller y a la vista

--- Properties/datossController.cs
+++ Properties/datossController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC_P11_EsmeraldaGarcía.Models;
 
 namespace MVC_P11_EsmeraldaGarcía.Properties
 {
@@ -6,7 +7,34 @@ namespace MVC_P11_EsmeraldaGarcía.Properties
     {
         public IActionResult Index()
         {
+            var listadodedato = (from e in _datoContext.dato
+                                 select new
+                                 {
+                                     Nombre = e.name_user,
+                                     Descripcion = e.direction,
+                                     Genero = e.genero,
+                                     Cursos = e.id_cursos,
+                                     Pasatiempo = e.pasatiempo
+                                 }).ToList();
+            ViewData["listadodedato"] = listadodedato;
             return View();
         }
+        public IActionResult Crear(dato nuevodato)
+        {
+            _datoContext.Add(nuevodato);
+            _datoContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+
+        private readonly datoContext _datoContext;
+        public datossController(datoContext datoContext)
+        {
+            _datoContext = datoContext;
+        }
+
     }
 }
+
